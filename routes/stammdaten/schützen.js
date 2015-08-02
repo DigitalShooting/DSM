88b095d39a17565bd4a7a4932aafe58c5ef803da
Jannik Lorenz <dev@janniklorenz.de>
2015-08-02 21:22:27 +0200

    switch to mongodb

--- routes/stammdaten/schützen.js
+++ routes/stammdaten/schützen.js
@@ -2,41 +2,46 @@ var express = require("express");
 var router = express.Router();
 var fs = require("fs")
 var path = require("path")
-var connection = require("../../lib/connection")
+
+var ObjectID = require("mongodb").ObjectID
+var database
+var mongodb = require("../../lib/mongodb")(function(db){
+	database = db
+})
 
 
 
 router.get("/", function(req, res){
-	connection.query(
-		"SELECT schuetzen.*, vereine.name as 'verein' " +
-		"FROM schuetzen " +
-		"LEFT OUTER JOIN vereine " +
-		"ON schuetzen.vereinID = vereine.id ",
-		function(err, schuetzen){
-			if (err){
-				console.error(err)
-			}
-			res.locals.schuetzen = schuetzen
-			res.render("stammdaten/schuetzen")
+	collection = database.collection('schuetzen')
+	collection.find().toArray(function(err, results) {
+		res.locals.schuetzen = results
+		res.render("stammdaten/schuetzen")
+	})
 
-		}
-	)
+	// connection.query(
+	// 	"SELECT schuetzen.*, vereine.name as 'verein' " +
+	// 	"FROM schuetzen " +
+	// 	"LEFT OUTER JOIN vereine " +
+	// 	"ON schuetzen.vereinID = vereine.id ",
+	// 	function(err, schuetzen){
+	// 		if (err){
+	// 			console.error(err)
+	// 		}
+	// 		res.locals.schuetzen = schuetzen
+	// 		res.render("stammdaten/schuetzen")
+	//
+	// 	}
+	// )
 })
 
 
 
 router.get("/edit/*", function(req, res, next){
-	connection.query(
-		"SELECT * " +
-		"FROM vereine ",
-		function(err, vereine){
-			if (err){
-				console.error(err)
-			}
-			res.locals.vereine = vereine
-			next()
-		}
-	)
+	collection = database.collection('vereine')
+	collection.find().toArray(function(err, results) {
+		res.locals.vereine = results
+		next()
+	})
 })
 
 router.get("/edit/new", function(req, res){
@@ -44,70 +49,38 @@ router.get("/edit/new", function(req, res){
 })
 
 router.post("/edit/new", function(req, res){
-	console.log(req.body.name)
-	connection.query(
-		"INSERT INTO schuetzen (name, vorname, passnummer, vereinID) " +
-		"VALUES (?, ?, ?, ?) ",
-		[req.body.name, req.body.vorname, req.body.passnummer, req.body.verein],
-		function(err, plugins){
-			if (err){
-				console.error(err)
-			}
-			res.redirect("../")
-		}
-	)
+	collection = database.collection('schuetzen')
+	collection.insert(req.body, function(err, docs) {
+		res.redirect("../")
+	})
 })
 
 router.get("/edit/:id", function(req, res){
-	connection.query(
-		"SELECT * " +
-		"FROM schuetzen " +
-		"WHERE id = ? ",
-		[req.params.id],
-		function(err, schuetzen){
-			if (err){
-				console.error(err)
-			}
-			if(schuetzen.length >= 1){
-				res.locals.schuetze = schuetzen[0]
-				res.render("stammdaten/schuetzen/edit")
-			} else {
-				res.redirect("../")
-			}
+	collection = database.collection('schuetzen')
+	collection.find({"_id": new ObjectID(req.params.id)}).toArray(function(err, results) {
+		if(results.length >= 1){
+			res.locals.schuetze = results[0]
+			res.render("stammdaten/schuetzen/edit")
+		} else {
+			res.redirect("../")
 		}
-	)
+	})
 })
 
 router.post("/edit/:id", function(req, res){
-	connection.query(
-		"UPDATE schuetzen " +
-		"SET name = ?, vorname = ?, passnummer = ?, vereinID = ? " +
-		"WHERE id = ? ",
-		[req.body.name, req.body.vorname, req.body.passnummer, req.body.verein, req.params.id],
-		function(err){
-			if (err){
-				console.error(err)
-			}
-			res.redirect("../")
-		}
-	)
+	collection = database.collection('schuetzen')
+	collection.update({"_id": new ObjectID(req.params.id)}, req.body, function(err){
+		res.redirect("../")
+	})
 })
 
 
 
 router.get("/delete/:id", function(req, res){
-	connection.query(
-		"DELETE FROM schuetzen " +
-		"WHERE id = ? ",
-		[req.params.id],
-		function(err){
-			if (err){
-				console.error(err)
-			}
-			res.redirect("../")
-
-		}
-	)
+	collection = database.collection('schuetzen')
+	collection.remove({"_id": new ObjectID(req.params.id)}, function(err){
+		res.redirect("../")
+	})
 })
 
 
--- routes/stammdaten/vereine.js
+++ routes/stammdaten/vereine.js
@@ -2,23 +2,19 @@ var express = require("express");
 var router = express.Router();
 var fs = require("fs")
 var path = require("path")
-var connection = require("../../lib/connection")
 
+var ObjectID = require("mongodb").ObjectID
+var collection
+var mongodb = require("../../lib/mongodb")(function(db){
+	collection = db.collection('vereine')
+})
 
 
 router.get("/", function(req, res){
-	connection.query(
-		"SELECT * " +
-		"FROM vereine ",
-		function(err, vereine){
-			if (err){
-				console.error(err)
-			}
-			res.locals.vereine = vereine
-			res.render("stammdaten/vereine")
-
-		}
-	)
+	collection.find().toArray(function(err, results) {
+		res.locals.vereine = results
+		res.render("stammdaten/vereine")
+	})
 })
 
 
@@ -28,69 +24,34 @@ router.get("/edit/new", function(req, res){
 })
 
 router.post("/edit/new", function(req, res){
-	connection.query(
-		"INSERT INTO vereine (name) " +
-		"VALUES (?) ",
-		[req.body.name],
-		function(err, plugins){
-			if (err){
-				console.error(err)
-			}
-			res.redirect("../")
-		}
-	)
+	collection.insert(req.body, function(err, docs) {
+		res.redirect("../")
+	})
 })
 
 router.get("/edit/:id", function(req, res){
-	connection.query(
-		"SELECT * " +
-		"FROM vereine " +
-		"WHERE id = ? ",
-		[req.params.id],
-		function(err, vereine){
-			if (err){
-				console.error(err)
-			}
-			if(vereine.length >= 1){
-				res.locals.verein = vereine[0]
-				res.render("stammdaten/vereine/edit")
-			} else {
-				res.redirect("../")
-			}
+	collection.find({"_id": new ObjectID(req.params.id)}).toArray(function(err, results) {
+		if(results.length >= 1){
+			res.locals.verein = results[0]
+			res.render("stammdaten/vereine/edit")
+		} else {
+			res.redirect("../")
 		}
-	)
+	})
 })
 
 router.post("/edit/:id", function(req, res){
-	connection.query(
-		"UPDATE vereine " +
-		"SET name = ? " +
-		"WHERE id = ? ",
-		[req.body.name, req.params.id],
-		function(err){
-			if (err){
-				console.error(err)
-			}
-			res.redirect("../")
-		}
-	)
+	collection.update({"_id": new ObjectID(req.params.id)}, req.body, function(err){
+		res.redirect("../")
+	})
 })
 
 
 
 router.get("/delete/:id", function(req, res){
-	connection.query(
-		"DELETE FROM vereine " +
-		"WHERE id = ? ",
-		[req.params.id],
-		function(err){
-			if (err){
-				console.error(err)
-			}
-			res.redirect("../")
-
-		}
-	)
+	collection.remove({"_id": new ObjectID(req.params.id)}, function(err){
+		res.redirect("../")
+	})
 })
 
 
