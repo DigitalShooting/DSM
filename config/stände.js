2bf7d8f537aac38bc76358a83f5d68eeee616904
Jannik Lorenz <dev@janniklorenz.de>
2015-08-06 02:35:21 +0200

    change config

--- config/stände.js
+++ config/stände.js
@@ -1,26 +1,26 @@
 module.exports = {
-	// stand1 : {
-	// 	label: "Stand 1",
-	// 	ip: "10.0.0.101",
-	// 	port: "3000",
-	// 	mac: "",
-	// },
-	// stand2 : {
-	// 	label: "Stand 2",
-	// 	ip: "10.0.0.102",
-	// 	port: "3000",
-	// 	mac: "",
-	// },
+	stand1 : {
+		label: "Stand 1",
+		ip: "10.0.1.1",
+		port: "3000",
+		mac: "",
+	},
+	stand2 : {
+		label: "Stand 2",
+		ip: "10.0.1.2",
+		port: "3000",
+		mac: "",
+	},
 	stand3 : {
 		label: "Stand 3",
 		ip: "10.0.1.3",
 		port: "3000",
 		mac: "",
 	},
-	// stand4 : {
-	// 	label: "Stand 4",
-	// 	ip: "10.0.0.104",
-	// 	port: "3000",
-	// 	mac: "",
-	// }
+	stand4 : {
+		label: "Stand 4",
+		ip: "10.0.1.4",
+		port: "3000",
+		mac: "",
+	}
 }
