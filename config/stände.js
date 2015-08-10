cd601a6524433faf3180ef7820172ee3b8cf8229
Jannik Lorenz <dev@janniklorenz.de>
2015-08-10 18:29:13 +0200

    add mac

--- config/stände.js
+++ config/stände.js
@@ -11,27 +11,27 @@ module.exports = {
 		label: "Stand 1",
 		ip: "10.0.1.1",
 		port: "3000",
-		mac: "",
+		mac: "00:12:3f:94:6d:13",
 	},
 	stand2 : {
 		_id: "stand2",
 		label: "Stand 2",
 		ip: "10.0.1.2",
 		port: "3000",
-		mac: "",
+		mac: "00:12:3f:94:70:22",
 	},
 	stand3 : {
 		_id: "stand3",
 		label: "Stand 3",
 		ip: "10.0.1.3",
 		port: "3000",
-		mac: "",
+		mac: "00:12:3f:93:e1:70",
 	},
 	stand4 : {
 		_id: "stand4",
 		label: "Stand 4",
 		ip: "10.0.1.4",
 		port: "3000",
-		mac: "",
+		mac: "00:12:3f:94:60:cb",
 	}
 }
