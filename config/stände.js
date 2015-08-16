e1574ea6c8e7f216d68c6389d9c57ea1026f81a1
Jannik Lorenz <dev@janniklorenz.de>
2015-08-16 18:36:28 +0200

    add 5-12 config

--- config/stände.js
+++ config/stände.js
@@ -39,4 +39,68 @@ module.exports = {
 		port: "3000",
 		mac: "00:12:3f:94:60:cb",
 	}
+	stand5 : {
+		_id: "stand5",
+		label: "Stand 5",
+		user: "diana",
+		ip: "10.0.1.5",
+		port: "3000",
+		mac: "00:12:3f:94:78:d1",
+	}
+	stand6 : {
+		_id: "stand6",
+		label: "Stand 6",
+		user: "diana",
+		ip: "10.0.1.6",
+		port: "3000",
+		mac: "00:12:3f:94:7b:ce",
+	}
+	stand7 : {
+		_id: "stand7",
+		label: "Stand 7",
+		user: "diana",
+		ip: "10.0.1.10",
+		port: "3000",
+		mac: "00:12:3f:94:6d:69",
+	}
+	stand8 : {
+		_id: "stand8",
+		label: "Stand 8",
+		user: "diana",
+		ip: "10.0.1.8",
+		port: "3000",
+		mac: "00:12:3f:94:11:ce",
+	}
+	stand9 : {
+		_id: "stand9",
+		label: "Stand 9",
+		user: "diana",
+		ip: "10.0.1.9",
+		port: "3000",
+		mac: "00:12:3f:93:86:6c",
+	}
+	stand10 : {
+		_id: "stand10",
+		label: "Stand 10",
+		user: "diana",
+		ip: "10.0.1.10",
+		port: "3000",
+		mac: "00:12:3f:94:6d:54",
+	}
+	stand11 : {
+		_id: "stand11",
+		label: "Stand 11",
+		user: "diana",
+		ip: "10.0.1.11",
+		port: "3000",
+		mac: "",
+	}
+	stand12 : {
+		_id: "stand12",
+		label: "Stand 12",
+		user: "diana",
+		ip: "10.0.1.12",
+		port: "3000",
+		mac: "00:12:3f:94:69:6d",
+	}
 }
