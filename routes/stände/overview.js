20292b125827f577834985533bfd0cead894d968
Jannik Lorenz <dev@janniklorenz.de>
2015-08-30 22:57:00 +0200

    improvments

--- routes/stände/overview.js
+++ routes/stände/overview.js
@@ -4,9 +4,26 @@ var fs = require("fs")
 var path = require("path")
 var config = require("../../config/")
 
+var ObjectID = require("mongodb").ObjectID
+var database
+var mongodb = require("../../lib/mongodb")(function(db){
+	database = db
+})
+
 router.use("/", function(req, res, next){
 	res.locals.stände = config.stände
-	next()
+
+	var collection = database.collection('vereine')
+	collection.find().toArray(function(err, results) {
+		res.locals.vereine = results
+
+		collection = database.collection('schuetzen')
+		collection.find().toArray(function(err, results) {
+			res.locals.schuetzen = results
+
+			next()
+		})
+	})
 })
 
 router.get("/", function(req, res){
