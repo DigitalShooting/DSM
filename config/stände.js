889e90ed2c53b257ea000da00cddcd2f7b60f495
Jannik Lorenz <dev@janniklorenz.de>
2015-08-30 22:01:55 +0200

    redesign stände page

--- config/stände.js
+++ config/stände.js
@@ -1,15 +1,46 @@
 module.exports = {
-	standTest : {
-		_id: "standTest",
-		label: "Stand Test",
+	standTest1 : {
+		_id: "standTest1",
+		label: "Stand Test 1",
+		labelShort: "T1",
 		user: "firebug",
 		ip: "127.0.0.1",
 		port: "3001",
 		mac: "",
 	},
+	standTest2 : {
+		_id: "standTest2",
+		label: "Stand Test 2",
+		labelShort: "T2",
+		user: "firebug",
+		ip: "127.0.0.1",
+		port: "3002",
+		mac: "",
+	},
+	standTest3 : {
+		_id: "standTest3",
+		label: "Stand Test 3",
+		labelShort: "T3",
+		user: "firebug",
+		ip: "127.0.0.1",
+		port: "3003",
+		mac: "",
+	},
+	standTest4 : {
+		_id: "standTest4",
+		label: "Stand Test 4",
+		labelShort: "T4",
+		user: "firebug",
+		ip: "127.0.0.1",
+		port: "3004",
+		mac: "",
+	},
+
+
 	stand1 : {
 		_id: "stand1",
 		label: "Stand 1",
+		labelShort: "1",
 		user: "diana",
 		ip: "10.0.1.1",
 		port: "3000",
@@ -18,6 +49,7 @@ module.exports = {
 	stand2 : {
 		_id: "stand2",
 		label: "Stand 2",
+		labelShort: "2",
 		user: "diana",
 		ip: "10.0.1.2",
 		port: "3000",
@@ -26,6 +58,7 @@ module.exports = {
 	stand3 : {
 		_id: "stand3",
 		label: "Stand 3",
+		labelShort: "3",
 		user: "diana",
 		ip: "10.0.1.3",
 		port: "3000",
@@ -34,6 +67,7 @@ module.exports = {
 	stand4 : {
 		_id: "stand4",
 		label: "Stand 4",
+		labelShort: "4",
 		user: "diana",
 		ip: "10.0.1.4",
 		port: "3000",
@@ -42,6 +76,7 @@ module.exports = {
 	stand5 : {
 		_id: "stand5",
 		label: "Stand 5",
+		labelShort: "5",
 		user: "diana",
 		ip: "10.0.1.5",
 		port: "3000",
@@ -50,6 +85,7 @@ module.exports = {
 	stand6 : {
 		_id: "stand6",
 		label: "Stand 6",
+		labelShort: "6",
 		user: "diana",
 		ip: "10.0.1.6",
 		port: "3000",
@@ -58,6 +94,7 @@ module.exports = {
 	stand7 : {
 		_id: "stand7",
 		label: "Stand 7",
+		labelShort: "7",
 		user: "diana",
 		ip: "10.0.1.7",
 		port: "3000",
@@ -66,6 +103,7 @@ module.exports = {
 	stand8 : {
 		_id: "stand8",
 		label: "Stand 8",
+		labelShort: "8",
 		user: "diana",
 		ip: "10.0.1.8",
 		port: "3000",
@@ -74,6 +112,7 @@ module.exports = {
 	stand9 : {
 		_id: "stand9",
 		label: "Stand 9",
+		labelShort: "9",
 		user: "diana",
 		ip: "10.0.1.9",
 		port: "3000",
@@ -82,6 +121,7 @@ module.exports = {
 	stand10 : {
 		_id: "stand10",
 		label: "Stand 10",
+		labelShort: "10",
 		user: "diana",
 		ip: "10.0.1.10",
 		port: "3000",
@@ -90,6 +130,7 @@ module.exports = {
 	stand11 : {
 		_id: "stand11",
 		label: "Stand 11",
+		labelShort: "11",
 		user: "diana",
 		ip: "10.0.1.11",
 		port: "3000",
@@ -98,6 +139,7 @@ module.exports = {
 	stand12 : {
 		_id: "stand12",
 		label: "Stand 12",
+		labelShort: "12",
 		user: "diana",
 		ip: "10.0.1.12",
 		port: "3000",
