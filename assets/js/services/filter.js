14362530a341375f19a170684e6b9c495d42d4b9
Jannik Lorenz <dev@janniklorenz.de>
2015-09-03 15:59:18 +0200

    fix filterjs

--- assets/js/services/filter.js
+++ assets/js/services/filter.js
@@ -8,10 +8,12 @@ angular.module("dsm.services.filter", [])
 				var itemMatches = false;
 				for (var i = 0; i < keys.length; i++) {
 					var prop = keys[i];
-					var text = props[prop].toLowerCase();
-					if (item[prop].toString().toLowerCase().indexOf(text) !== -1) {
-						itemMatches = true;
-						break;
+					if (props[prop] != undefined){
+						var text = props[prop].toLowerCase();
+						if (item[prop].toString().toLowerCase().indexOf(text) !== -1) {
+							itemMatches = true;
+							break;
+						}
 					}
 				}
 				if (itemMatches) {
