c42276f94084d044d1c4365a05eafdf0eee94e92
Jannik Lorenz <dev@janniklorenz.de>
2015-09-09 04:42:39 +0200

    huge cleanup

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-/* @flow */
-
 var express = require("express")
 var less = require("less")
 var compression = require('compression')
@@ -12,56 +10,56 @@ var exec = require("exec")
 var child_process = require('child_process')
 var ping = require('ping')
 
-var app = express({ strict: true })
 
+
+
+var app = express({ strict: true })
 
 app.use(function(req, res, next){
 	res.locals.req = req
 	next()
 })
 
+// set up jade
 app.set('view engine', 'jade');
 
+// route fonts
 app.use("/fonts/", express.static("./node_modules/bootstrap/fonts"))
 
-app.use("/js/socket.io.js", express.static("./node_modules/socket.io/node_modules/socket.io-client/socket.io.js"))
-
+// route js/libs
 app.use("/js/", express.static(__dirname + "/assets/js"))
 app.use("/libs/", express.static(__dirname + "/assets/libs"))
-app.use("/js/", express.static(__dirname + "/node_modules/jquery/dist"))
 
+// route/render css
 app.use("/css/", lessMiddleware(__dirname + "/stylesheets"))
 app.use("/css/", express.static(__dirname + "/stylesheets"))
 
+// enable forms
 app.use(bodyParser.urlencoded({
 	extended: true
 }))
 
+// enable compression
 app.use(compression())
 
 
 
+
+// Main routes
 app.use("/stammdaten/", routes.stammdaten)
 app.use("/rwks/", routes.rwks)
-app.use("/exit/", routes.exit)
-
-// app.use("/live/", routes.live)
-//
-// app.use("/manschaften", routes.manschaften);
-// app.use("/schuetzen", routes.schützen)
-// app.use("/vereine", routes.vereine)
-//
 app.use("/lines", routes.lines)
-// app.use("/auswertung", routes.auswertung)
 
-
-
+app.use("/exit/", routes.exit)
 
 app.get("/", function(req, res){
 	res.render("index.jade")
 })
 
 
+
+
+// Set up express & socket.io
 var server = http.Server(app)
 var io = require('socket.io')(server)
 server.listen(config.network.port, config.network.address)
@@ -72,19 +70,10 @@ server.on('listening', function() {
 
 
 
-
-
+// socket.io
 io.on('connection', function(socket){
 
-	for(var id in statusCache){
-		var line = config.lines[id]
-
-		socket.emit('setStatus', {
-			line: line,
-			alive: statusCache[id],
-		})
-	}
-
+	// set power performs wakeonlan or ssh shutdown on target machine
 	socket.on('setPower', function(data){
 		var line = config.lines[data.line]
 		if (data.state == true){
@@ -96,32 +85,4 @@ io.on('connection', function(socket){
 			child_process.exec(["ssh -t "+line.user+"@"+line.ip+" 'sudo shutdown -h now'"], function(err, out, code) { })
 		}
 	})
-
 })
-
-
-
-
-
-
-
-
-var lines = []
-for(var key in config.lines){
-	var line = config.lines[key]
-	lines.push(line)
-}
-
-var statusCache = {}
-
-setInterval(function(){
-	lines.forEach(function(line){
-		ping.sys.probe(line.ip, function(isAlive){
-			io.emit('setStatus', {
-				line: line,
-				alive: isAlive,
-			})
-			statusCache[line._id] = isAlive
-		})
-	})
-}, 5000)
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
 var routes = {}
 routes.stammdaten = require("./stammdaten/")
 routes.rwks = require("./rwks/")
-routes.exit = require("./exit/")
-
-// routes.live = require("./live")
 routes.lines = require("./lines/")
-// routes.auswertung = require("./auswertung")
+
+routes.exit = require("./exit/")
 
 module.exports = routes
