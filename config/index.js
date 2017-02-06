917628958e4405ccce778d706faf3d5b00edf485
Jannik Lorenz <dev@janniklorenz.de>
2017-02-06 19:51:48 +0100

    Add disziplinen submodule

--- config/index.js
+++ config/index.js
@@ -2,5 +2,5 @@ module.exports = {
 	database: require("./database.js"),
 	lines : require("./lines.js"),
 	network	: require("./network.js"),
-	disziplinen: require("./disziplinen/"),
+	disziplinen: require("../disziplinen/"),
 };
