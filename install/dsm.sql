1e66848a56e2d45094c62c042c51a64e7939537d
Jannik Lorenz <dev@janniklorenz.de>
2017-03-27 01:07:16 +0200

    Update sql init

--- install/dsm.sql
+++ install/dsm.sql
@@ -1,63 +1,17 @@
--- Adminer 4.2.2 MySQL dump
+-- Adminer 4.2.5 MySQL dump
 
 SET NAMES utf8;
 SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 DROP TABLE IF EXISTS `manschaft`;
 CREATE TABLE `manschaft` (
   `name` text COLLATE utf8_unicode_ci NOT NULL,
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `vereinID` int(11) NOT NULL,
-  `saisonID` int(11) NOT NULL,
   `note` text COLLATE utf8_unicode_ci NOT NULL,
+  `anzahlSchuetzen` int(5) NOT NULL DEFAULT '4',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-
-DROP TABLE IF EXISTS `memberIn`;
-CREATE TABLE `memberIn` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `userID` int(11) NOT NULL,
-  `manschaftID` int(11) NOT NULL,
-  `stamm` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `userID_manschaftID` (`userID`,`manschaftID`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-
-DROP TABLE IF EXISTS `rwk`;
-CREATE TABLE `rwk` (
-  `manschaftHeim` int(11) NOT NULL,
-  `manschaftGast` int(11) NOT NULL,
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `date` date NOT NULL DEFAULT '0000-00-00',
-  `saisonID` int(11) NOT NULL,
-  `note` text COLLATE utf8_unicode_ci NOT NULL,
-  `done` tinyint(1) unsigned zerofill NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-
-DROP TABLE IF EXISTS `saison`;
-CREATE TABLE `saison` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` text COLLATE utf8_unicode_ci NOT NULL,
-  `note` text COLLATE utf8_unicode_ci NOT NULL,
-  `disziplinID` tinytext COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-
-DROP TABLE IF EXISTS `shotIn`;
-CREATE TABLE `shotIn` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `userID` int(11) NOT NULL,
-  `rwkID` int(11) NOT NULL,
-  `ersatz` tinyint(1) NOT NULL,
-  `gast` tinyint(1) NOT NULL,
-  `dataID` text COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `userID_rwkID_gast` (`userID`,`rwkID`,`gast`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
@@ -96,4 +50,4 @@ CREATE TABLE `verein` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
--- 2016-06-12 15:01:20
+-- 2017-03-26 23:06:34
